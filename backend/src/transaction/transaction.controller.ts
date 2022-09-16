ca2ae7d87ce93939b36eb6e12ea6f59fcb0e0b01
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-17 03:40:38 +0800

    Implement response service in transaction resource

--- backend/src/transaction/transaction.controller.ts
+++ backend/src/transaction/transaction.controller.ts
@@ -1,66 +1,59 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
-import { CreateTransactionDto } from '../interfaces';
+import {
+  BaseItemDto,
+  BaseTransactionDto,
+  CreateTransactionDto,
+} from '../interfaces';
 import { InjectConnection } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
-import { STATUS, MESSAGE } from '../constant';
+import { RESOURCE, METHOD } from '../constant';
+import { ResponseService } from 'src/services/response/response.service';
 
 @Controller('transaction')
 export class TransactionController {
   constructor(
     private readonly service: TransactionService,
+    private readonly response: ResponseService,
     @InjectConnection() private readonly connection: mongoose.Connection,
   ) {}
 
   @Post()
-  async create(@Body() dto: CreateTransactionDto) {
-    let response = {
-      status: STATUS.SUCCESS,
-      message: MESSAGE.SUCCESS_CREATE,
-      data: [],
-    };
+  async create(@Body() dto: CreateTransactionDto<BaseItemDto>) {
+    let message = {};
 
     const session = await this.connection.startSession();
 
-    await session
-      .withTransaction(async () => {
-        const data = await this.service.create(dto, session);
-        response = {
-          status: STATUS.SUCCESS,
-          message: MESSAGE.SUCCESS_CREATE,
-          data: [data],
-        };
-      })
-      .catch((e) => {
-        response = {
-          status: STATUS.FAIL,
-          message: e,
-          data: [],
-        };
-      });
+    await session.withTransaction(async () => {
+      const data = await this.service.create(dto, session);
+      message = this.response.success<BaseTransactionDto<any>>(
+        RESOURCE.TRANSACTION,
+        METHOD.CREATE,
+        data,
+      );
+    });
 
     session.endSession();
-
-    return response;
+    return message;
   }
 
   @Get()
   async findAll() {
     const data = await this.service.findAll();
-    return {
-      status: STATUS.SUCCESS,
-      message: MESSAGE.SUCCESS_FETCH,
+    return this.response.success<BaseTransactionDto<any>>(
+      RESOURCE.TRANSACTION,
+      METHOD.FETCH,
       data,
-    };
+    );
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
     const data = await this.service.findOne(id);
-    return {
-      status: STATUS.SUCCESS,
-      message: MESSAGE.SUCCESS_FETCH,
-      data: [data],
-    };
+    return this.response.success<BaseTransactionDto<any>>(
+      RESOURCE.TRANSACTION,
+      METHOD.FETCH,
+      [data],
+    );
   }
 }
--- backend/src/transaction/transaction.module.ts
+++ backend/src/transaction/transaction.module.ts
@@ -3,10 +3,11 @@ import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Transaction, TransactionSchema } from './entities/transaction.entity';
+import { ResponseService } from 'src/services/response/response.service';
 
 @Module({
   controllers: [TransactionController],
-  providers: [TransactionService],
+  providers: [TransactionService, ResponseService],
   imports: [
     MongooseModule.forFeature([
       { name: Transaction.name, schema: TransactionSchema },
