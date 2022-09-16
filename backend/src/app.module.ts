f0395231fe2171d53f0d9e54522ba4da8114d868
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-16 22:11:27 +0800

    Setup environments

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import environment from './config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(environment.MONGO_DB),
+    ConfigModule.forRoot({
+      load: [() => ({ environment })],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
