f76afd94bc293efec1401ca7040944bb62aa7265
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-17 07:25:20 +0800

    Add transaction page

---
+++ frontend/src/app/pages/transaction-page/transaction-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TransactionPageComponent } from './transaction-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    TransactionPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TransactionPageModule { }
