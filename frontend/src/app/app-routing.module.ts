10861c040d504c64864e7969d4b8f7179157aafe
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-17 20:18:37 +0800

    Add routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'item',
+    loadChildren: () =>
+      import('./pages/item-page/item-page.module').then(
+        (m) => m.ItemPageModule
+      ),
+  },
+  {
+    path: 'home',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
