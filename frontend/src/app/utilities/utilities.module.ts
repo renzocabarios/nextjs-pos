1427f44bf154eddc528509a4fbafb3a92a9ccf74
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-17 09:51:51 +0800

    Add export in utilities

--- frontend/src/app/utilities/utilities.module.ts
+++ frontend/src/app/utilities/utilities.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatatableComponent } from './datatable/datatable.component';
-
-
+import { ScreenLayoutComponent } from './screen-layout/screen-layout.component';
+import { MainBtnComponent } from './main-btn/main-btn.component';
 
 @NgModule({
-  declarations: [
-    DatatableComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [DatatableComponent, ScreenLayoutComponent, MainBtnComponent],
+  imports: [CommonModule],
+  exports: [DatatableComponent, ScreenLayoutComponent, MainBtnComponent],
 })
-export class UtilitiesModule { }
+export class UtilitiesModule {}
