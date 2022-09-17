aab9035294bc3ae2d477d6829b6b6b282b3fc3c8
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-17 09:52:42 +0800

    Add interfaces

---
+++ frontend/src/app/interfaces/index.ts
@@ -0,0 +1,39 @@
+export interface IBaseModel {
+  _id?: string;
+  deleted: boolean;
+}
+
+export interface IItem extends IBaseModel {
+  name: string;
+  basePrice: number;
+}
+
+export interface IItemState {
+  entries: IItem[];
+}
+
+export type ResponseStatus = 'success' | 'fail';
+export type ResponseMessage = string;
+export type ResponseData<T> = T[];
+
+export interface IHttpResponse<T> {
+  status: ResponseStatus;
+  message: ResponseMessage;
+  data: ResponseData<T>;
+}
+
+export type DatatableHeader = string[];
+export type DatatableKeys = string[];
+export type DatatableData<T> = T[];
+
+export interface IAction {
+  title: string;
+  onClick: (_id: string) => void;
+}
+
+export interface IDatatable<T> {
+  header: string[];
+  keys: string[];
+  data: DatatableData<T>;
+  actions: IAction[];
+}
