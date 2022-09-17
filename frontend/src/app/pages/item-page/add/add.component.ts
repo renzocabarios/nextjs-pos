5aace222b0a2726432411072558b3f1b3105efee
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-17 23:19:18 +0800

    Add add item page

--- frontend/src/app/pages/item-page/add/add.component.ts
+++ frontend/src/app/pages/item-page/add/add.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { createItem } from '../../../states/item/item.action';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  formGroup = new FormGroup({
+    name: new FormControl(''),
+    basePrice: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor(private store: Store) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submit(): void {
+    const { name, basePrice } = this.formGroup.value;
+    this.store.dispatch(
+      createItem({
+        data: {
+          name: name ?? '',
+          basePrice: Number(basePrice),
+        },
+      })
+    );
   }
-
 }
