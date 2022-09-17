a2e70064395bdd03f96008aa969b04aac8d9a432
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-09-17 20:19:04 +0800

    Update item module routing

--- frontend/src/app/pages/item-page/item-page.module.ts
+++ frontend/src/app/pages/item-page/item-page.module.ts
@@ -3,17 +3,37 @@ import { CommonModule } from '@angular/common';
 import { ItemPageComponent } from './item-page.component';
 import { AddComponent } from './add/add.component';
 import { EditComponent } from './edit/edit.component';
+import { RouterModule, Routes } from '@angular/router';
+import { UtilitiesModule } from 'src/app/utilities/utilities.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: ItemPageComponent,
+  },
+  {
+    path: 'add',
+    component: AddComponent,
+  },
+  {
+    path: ':id/edit',
+    component: EditComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
-  declarations: [
-    ItemPageComponent,
-    AddComponent,
-    EditComponent
-  ],
+  declarations: [ItemPageComponent, AddComponent, EditComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes),
+    UtilitiesModule,
+    ReactiveFormsModule,
+  ],
 })
-export class ItemPageModule { }
+export class ItemPageModule {}
