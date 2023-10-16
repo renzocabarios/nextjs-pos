d82869cf8417ec58ee3a1bd583e745a895ba73cf
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-16 21:08:08 +0800

    Update ItemCard

--- components/ItemCard/index.tsx
+++ components/ItemCard/index.tsx
@@ -8,7 +8,7 @@ export default function ItemCard({ name, price, onClick }: any) {
     <>
       <div className={style.container} onClick={onClick ?? (() => {})}>
         <Image src={image} alt="TEST" />
-        <p className="text-white-900 font-bold">{name}</p>
+        <p className="text-white-200 font-bold">{name}</p>
         <h5 className="text-secondary-500">₱{price}.00</h5>
       </div>
     </>
