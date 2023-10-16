1b94b19de50df6f51cc606a9cf0a279f6dd55e6a
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-16 16:27:57 +0800

    Add tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        xs: "0.5625rem",
+        sm: "0.6875rem",
+        md: "1rem",
+        xl: "1.1875rem",
+        h6: "1.375rem",
+        h5: "1.6875rem",
+        h4: "2.125rem",
+        h3: "2.6875rem",
+        h2: "3.3125rem",
+        h1: "4.1875rem",
+      },
       colors: {
         primary: {
           100: "#d0e9ff",
