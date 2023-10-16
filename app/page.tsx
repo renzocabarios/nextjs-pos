3d38e6511177804e86b5d4f372154c80193ce4fc
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-16 20:05:17 +0800

    Update home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
-import { Sidenav } from "@/components";
+"use client";
+import { Sidenav, ItemCard } from "@/components";
+import { useEffect } from "react";
+import { useItemsStore } from "@/states";
+import style from "./style.module.css";
 
 export default function Home() {
+  const { getItems, data } = useItemsStore() as any;
+
+  useEffect(() => {
+    getItems();
+  });
+
   return (
     <main>
       <Sidenav>
-        <h1>asd</h1>
+        <div className={style.container}>
+          {data.map((e: any) => {
+            return <ItemCard key={e._id} name={e.name} price={e.price} />;
+          })}
+        </div>
       </Sidenav>
     </main>
   );
