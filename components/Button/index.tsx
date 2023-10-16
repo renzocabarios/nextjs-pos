fa22473634b259e5682c49560057d7281d6ef3f9
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-16 16:28:26 +0800

    Add Button

---
+++ components/Button/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import style from "./style.module.css";
+
+export default function Button({
+  onClick,
+  type,
+  children,
+  outlined,
+  light,
+}: any) {
+  return (
+    <>
+      <button
+        className={`${style.button} ${
+          outlined ? style.outlined : style.filled
+        } ${light ? style.light : style.default}`}
+        onClick={onClick}
+        type={type ?? "button"}
+      >
+        {children}
+      </button>
+    </>
+  );
+}
