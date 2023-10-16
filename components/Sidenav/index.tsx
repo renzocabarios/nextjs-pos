fc47fc9e0b607488cf9ebff2b00a8f471fd0e4b8
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-16 20:55:07 +0800

    Update SideNav

--- components/Sidenav/index.tsx
+++ components/Sidenav/index.tsx
@@ -1,11 +1,26 @@
 "use client";
 import React from "react";
 import style from "./style.module.css";
+import { useTransactionsStore } from "@/states";
 
 export default function Sidenav({ children }: any) {
+  const { items } = useTransactionsStore() as any;
+
   return (
     <>
-      <div className={style.sidenav}></div>
+      <div className={style.sidenav}>
+        {items.map((e: any) => {
+          return (
+            <div className={style.item_container} key={e._id}>
+              <div className={style.row}>
+                <h5 className="text-white-900">{e.name}</h5>
+                <p className="text-secondary-500">2x</p>
+              </div>
+              <h5 className="text-secondary-500">₱{e.price}.00</h5>
+            </div>
+          );
+        })}
+      </div>
       <div className={style.container}>
         <div className={style.divider}></div>
         <div className={style.content}>{children}</div>
