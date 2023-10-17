8a5b1e4a0fa31030d64ac4996bc6ea2b4328b777
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-17 13:58:18 +0800

    Update Navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -2,14 +2,17 @@
 import React from "react";
 import style from "./style.module.css";
 import { Button } from "@/components";
-
+import { useRouter } from "next/navigation";
 export default function Navbar() {
+  const router = useRouter();
+
   return (
     <>
       <div className={style.container}>
-        <Button>Transactions</Button>
-        <div></div>
-        <div></div>
+        <Button onClick={() => router.push("/")}>Create Transactions</Button>{" "}
+        <Button onClick={() => router.push("/transactions")}>
+          Transactions
+        </Button>
       </div>
       <div className={style.divider}></div>
     </>
